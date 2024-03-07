29dcb3c1ff1ba6a8337c9b21f7abef7a605b0a07
Joshua Hamilton <dachs218@yahoo.com>
2024-03-07 16:30:17 -0600

    Test commit

---
+++ SafeSkate/Tests/Class2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class Class2
+    {
+        static bool Main(string[] args)
+        {
+            def testVerifyLocation(coordinate c1)
+            {
+                if (isDuplicateCoord(c1) == false​)
+                {
+                    if (isStreet(c1) == true)
+                        return true;
+                    else if (isNearStreet(c1) == true​)
+                        return true​;
+                    else return false​;
+                }
+                else return false;
+            }
+        }
+    }
+}
