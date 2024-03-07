fa8a797d13fabab05908997e5a747b95f6b4fbe7
DGdev001 <David.G.Norvell-1@ou.edu>
2024-03-07 17:41:03 -0600

    added reference to fix namespace errors

--- SafeSkate/Tests/VerifyLocation.cs
+++ SafeSkate/Tests/VerifyLocation.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SafeSkate;
+
 
 namespace Tests
 {
+   
     class VerifyLocation
     {
+        
+
         static bool testVerifyLocation(Coordinate c1)
         {
             if (isDuplicateCoord(c1) == false)
