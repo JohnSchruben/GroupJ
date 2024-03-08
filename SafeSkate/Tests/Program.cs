c8cef8f369aa25acabec88aa15164663952342ec
JohnSchruben <84052599+JohnSchruben@users.noreply.github.com>
2024-03-07 21:43:44 -0600

    test example

--- SafeSkate/Tests/Program.cs
+++ SafeSkate/Tests/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Tests
+﻿using SafeSkate;
+using System.Runtime.CompilerServices;
+using Unity;
+
+namespace Tests
 {
     class Program
     {
         public static void Main(string[] args)
         {
+            IUnityContainer container = new UnityContainer();
+            container.AddExtension(new TestContainerExtension());
+
+            var tests = container.ResolveAll<ITest>();
+
+            foreach(var test in tests)
+            {
+                Console.WriteLine(test.RunTest());
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ SafeSkate/Tests/TestExample.cs
@@ -0,0 +1,24 @@
+﻿using SafeSkate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class TestExample : ITest
+    {
+        private Coordinate coordinate;
+        public TestExample(Coordinate coordinate)
+        {
+            this.coordinate = coordinate;
+        }
+
+        public string RunTest()
+        {
+            // check something 
+            return $"the test was inconclusive...coordinate.X{coordinate.X},coordinate.Y{coordinate.Y}";
+        }
+    }
+}
