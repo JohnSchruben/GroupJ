64cb64dfe6b37baff29117dd314a5097b9153d8c
JohnSchruben <84052599+JohnSchruben@users.noreply.github.com>
2024-03-07 21:50:43 -0600

    added comments

--- SafeSkate/Tests/Program.cs
+++ SafeSkate/Tests/Program.cs
@@ -8,13 +8,18 @@ namespace Tests
     {
         public static void Main(string[] args)
         {
+            // using the container to register tests. we do this so that 
+            // you dont have to call new to constuct objects
             IUnityContainer container = new UnityContainer();
             container.AddExtension(new TestContainerExtension());
 
+            // resolve any class implementing ITest
             var tests = container.ResolveAll<ITest>();
 
+            // run each test
             foreach(var test in tests)
             {
+                // print test result
                 Console.WriteLine(test.RunTest());
             }
 
